a674de99448ad53782bfde16ee125712423022e2
den <den.velez@gmail.com>
2020-06-01 12:38:40 -0500

    home template was made in pages

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+  return `
+  <div class="Characters">
+    <article class="Charater-item">
+      <a href="#/1/">
+        <img src="image" alt="name">
+      </a>
+    </article>
+  </div>`;
+};
+
+export default Home;
