685f3bd104d01b15db58a4e6e6da5acfb8dcf46b
den <den.velez@gmail.com>
2020-06-01 15:02:32 -0500

    getHash and resolveRoutes were created

---
+++ src/utils/getHash.js
@@ -0,0 +1,8 @@
+const getHash = () =>
+  location.hash.slide(1).toLocateLowerCase().split("/")[1] || "/";
+//slide para eliminiar elementos (estamos tomando el dato de la ruta en el navegador)
+//toLocateLowerCase() para siempre tomar minuscula en caso que el usuario coloque mayuscula
+//split para dividir el string, crear un arreglo
+//[1] porque la primera posicion es 0 y es un "/"
+
+export default getHash;
